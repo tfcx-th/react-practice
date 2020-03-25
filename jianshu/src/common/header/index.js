0c0d581cd75850bb695bbd2dde005c076b6ca472
tfcx-th <tonghao_xjtu@qq.com>
2020-03-25 23:03:10 +0800

    commit

--- jianshu/src/common/header/index.js
+++ jianshu/src/common/header/index.js
@@ -1,11 +1,22 @@
 import React from 'react'
-import { HeaderWrapper, Logo } from './style'
+import { HeaderWrapper, Logo, Nav, NavItem, NavSearch, Addition, Button } from './style'
 
 class Header extends React.Component {
     render() {
         return (
             <HeaderWrapper>
                 <Logo href='/'/>
+                <Nav>
+                    <NavItem className="left active">首页</NavItem>
+                    <NavItem className="left">下载App</NavItem>
+                    <NavItem className="right">登录</NavItem>
+                    <NavItem className="right">Aa</NavItem>
+                    <NavSearch />
+                </Nav>
+                <Addition>
+                    <Button className="write">写文章</Button>
+                    <Button className="reg">注册</Button>
+                </Addition>
             </HeaderWrapper>
         )
     }
--- jianshu/src/common/header/style.js
+++ jianshu/src/common/header/style.js
@@ -16,3 +16,67 @@ export const Logo = styled.a`
     background: url(${logo});
     background-size: contain;
 `
+
+export const Nav = styled.div`
+    box-sizing: border-box;
+    margin: 0 150px 0 100px;
+    padding-right: 70px;
+    height: 100%;
+`
+
+export const NavItem = styled.div`
+    padding: 0 15px;
+    font-size: 17px;
+    line-height: 56px;
+    color: #333;
+    &.left {
+        float: left;
+    }
+    &.right {
+        float: right;
+        color: #969696;
+    }
+    &.active {
+        color: #ea6f5a;
+    }
+`
+
+export const NavSearch = styled.input.attrs({
+    placeholder: '搜索'
+})`
+    box-sizing: border-box;
+    margin: 9px 0 9px 20px;
+    padding: 0 20px;
+    width: 160px;
+    height: 38px;
+    border: none;
+    border-radius: 19px;
+    background: #eee;
+    font-size: 14px;
+    &::placeholder {
+        color: #999;
+    }
+`
+export const Addition = styled.div`
+    position: absolute;
+    right: 0;
+    top: 0;
+    height: 56px;
+`
+
+export const Button = styled.div`
+    float: right;
+    margin: 9px 20px 9px 0;
+    padding: 0 20px;
+    font-size: 14px;
+    line-height: 38px;
+    border: 1px solid #ec6149;
+    border-radius: 19px;
+    &.reg {
+        color: #ec6149;
+    }
+    &.write {
+        color: white;
+        background: #ec6149;
+    }
+`
